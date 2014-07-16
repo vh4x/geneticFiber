cd8a575ef2aab25c89fab3f4e7a505efadbfe270
Victor Araujo <templar996@gmail.com>
2014-07-16 12:40:32 -0600

    Directorios separados para run_betas

--- evaluador.f90
+++ evaluador.f90
@@ -14,8 +14,8 @@ USE GenerarMpb
       CHARACTER*7 fileFormat
       CHARACTER*15 betaFile
       CHARACTER*50 nanReplace
-      CHARACTER*30 mpbExecFormat
-      CHARACTER*30 runBetasExecFormat
+      CHARACTER*50 mpbExecFormat
+      CHARACTER*50 runBetasExecFormat
       CHARACTER*20 createLocal
       CHARACTER*20 cdLocal
       LOGICAL :: file_exists
@@ -43,22 +43,22 @@ SUBROUTINE dispersiones(my_rank, r1, r2, rg, re, dispersion)
       ELSE
       	 IF (my_rank.lt.10) THEN
       	 	write (fileFormat, "(A7)") "(A7,I1)"
-      	 	write (mpbExecFormat, *) "(A11, I1, A10, I1, A4)"
-      	 	write (runBetasExecFormat, *) "(A17, I1, A16, I1, A4)"
+      	 	write (mpbExecFormat, *) "(A11, I1, A5, I1, A8, I1, A4)"
+          write (runBetasExecFormat, *) "(A12, I1, A8, I1, A16, I1, A4)"
           write(betaFile, "(A5, I1, A4)") "betas", my_rank, ".out"
           write(createLocal, "(A6, I1)") "mkdir ", my_rank
           write(cdLocal, "(A3, I1)") "cd ", my_rank
       	 ELSE IF (my_rank.gt.9) THEN
       	 	write (fileFormat, "(A7)") "(A7,I2)"
-      	 	write (mpbExecFormat, *) "(A11, I2, A10, I2, A4)"
-      	 	write (runBetasExecFormat, *) "(A17, I2 A16, I2, A4)"
+      	 	write (mpbExecFormat, *) "(A11, I2, A5, I2, A8, I2, A4)"
+          write (runBetasExecFormat, *) "(A12, I2, A8, I2, A16, I2, A4)"
           write(betaFile, "(A5, I2, A4)") "betas", my_rank, ".out"
           write(createLocal, "(A6, I2)") "mkdir ", my_rank
           write(cdLocal, "(A3, I2)") "cd ", my_rank
       	 ELSE IF (my_rank.gt.99) THEN
       	 	write (fileFormat, "(A7)") "(A7,I3)"
-      	 	write (mpbExecFormat, *) "(A11, I3, A10, I3, A4)"
-      	 	write (runBetasExecFormat, *) "(A17, I3, A16, I3, A4)"
+      	 	write (mpbExecFormat, *) "(A11, I3, A5, I3, A8, I3, A4)"
+      	 	write (runBetasExecFormat, *) "(A12, I3, A8, I3, A16, I3, A4)"
           write(betaFile, "(A5, I3, A4)") "betas", my_rank, ".out"
           write(createLocal, "(A6, I3)") "mkdir ", my_rank
           write(cdLocal, "(A3, I3)") "cd ", my_rank
@@ -73,15 +73,15 @@ SUBROUTINE dispersiones(my_rank, r1, r2, rg, re, dispersion)
           !  write (*, *) "No existe el archivo de entrada ", paramFile
          !ELSE
             CALL SYSTEM (createLocal)
-            CALL SYSTEM (cdLocal)
+            !CALL SYSTEM (cdLocal)
             ! Llamada al procedimiento para generar el archivo MPB y luego llamadas para correrlo
             CALL escribirMpb(my_rank, 0.3, 0.3, 0.6, 0.6)
             file = ""
-            write(mpbExec, mpbExecFormat) "mpb mpbFile", my_rank, " > anillos", my_rank, ".out"
+            write(mpbExec, mpbExecFormat) "mpb mpbFile", my_rank, " > ./", my_rank, "/anillos", my_rank, ".out"
             CALL SYSTEM(mpbExec)
             write(*, *) mpbExec
 
-            write(runBetasExec, runBetasExecFormat) "run_betas anillos", my_rank, ".out 0.5 > betas", my_rank, ".out"
+            write(runBetasExec, runBetasExecFormat) "run_betas ./", my_rank,"/anillos", my_rank, ".out 0.5 > betas", my_rank, ".out"
             CALL SYSTEM(runBetasExec)
             write(*, *) runBetasExec
             CALL SYSTEM(nanReplace)
